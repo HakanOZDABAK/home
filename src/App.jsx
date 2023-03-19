272430480cb0501de57a71ec8c502b3b3329b893
Your Name <you@example.com>
2023-03-19 19:38:04 +0300

    First Changes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Container } from 'react-bootstrap';
+import './App.css';
+import Dashboard from './layouts/Dashboard';
+import Navi from './layouts/Navi.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navi/>
+    <Container>
+      <Dashboard/>
+    </Container>
+    </div>
+  );
+}
+
+export default App;
