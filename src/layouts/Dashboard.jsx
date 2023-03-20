bfa20b75ca9aece71631bce370c14f5ef87ad02b
Your Name <you@example.com>
2023-03-21 01:14:18 +0300

    changes2

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes} from 'react-router-dom'
+import Deneme from '../pages/deneme'
 import WhoIs from '../pages/WhoIs'
 
 export default function Dashboard() {
   return (
     <div >
-      <Routes>
-        <Route path="/" element={<WhoIs />} />
-      </Routes>
+    
+        <Routes>
+          <Route path="/" element={<WhoIs />} />
+          <Route path="/home2" element={<Deneme />} />
+        </Routes>
+  
     </div>
   )
 }
