502dc4ebbf26224868c9b5daade84218e9dc761f
Your Name <you@example.com>
2023-03-21 11:48:20 +0300

    fifth

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename=''>
+  <HashRouter >
     <App />
     </HashRouter>
 );
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Route,Router ,Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Deneme from '../pages/deneme'
 import WhoIs from '../pages/WhoIs'
 export default function Dashboard() {
   return (
     <div >
-      <Router>
-        <Switch>
-        <Route path="" component={<WhoIs />} />
-        <Route path="/" component={<WhoIs />} />
-        <Route path="/home2" component={<Deneme />} />
-        </Switch>
-      </Router>
+      <Routes>
+        <Route path="" element={<WhoIs/>}/>
+        <Route path="home" element={<WhoIs/>}/>
+        <Route path="home2" element={<Deneme/>}/>
+      </Routes>
     </div>
   )
 }
